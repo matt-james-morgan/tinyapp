ec46638a2588fb811316446d62eb13978699de17
Matt Morgan <matt.james.morgan97@gmail.com>
2023-11-24 18:04:20 +0000

    adds hello when home page is requested

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
