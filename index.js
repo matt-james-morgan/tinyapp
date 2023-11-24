9af4da6523ded914b12244752fbf026b288dc007
Matt Morgan <matt.james.morgan97@gmail.com>
2023-11-24 21:53:39 +0000

    adds crypto to required and implements genreate random number function

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const crypto = require("crypto")
 const app = express();
 const PORT = 3000;
 
@@ -6,6 +7,10 @@ const PORT = 3000;
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
+}
+function generateRandomString(num) {
+  return crypto.randomBytes(num).toString('hex');
+  
 }
 
 //uses ejs middlware
@@ -39,6 +44,11 @@ app.get("/urls/:id", (req, res)=>{
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, ()=>{
