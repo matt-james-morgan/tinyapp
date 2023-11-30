d9f482b68b9c06137b6f4cf312a0a4c3722aa983
Matt Morgan <matt.james.morgan97@gmail.com>
2023-11-30 03:01:49 +0000

    adds extra test to make sure function comes back undefined when user doesnt exist

--- test/helpertest.js
+++ test/helpertest.js
@@ -19,7 +19,11 @@ describe('getUserByEmail', function() {
   it('should return a user with valid email', function() {
     const user = getUserByEmail("user@example.com", testUsers)
     const expectedUserID = "userRandomID";
-    
     assert.deepEqual(user,expectedUserID, "user ids match");
+  });
+  it('should return undefined if it can not find user', function() {
+    const user = getUserByEmail("fakeuser@example.com", testUsers)
+    const expectedUserID = undefined;
+    assert.deepEqual(user,expectedUserID, "comes back undefined");
   });
 });
