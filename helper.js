23f203ac940f4d7bf038b0f00ecb660f3c810706
Matt Morgan <matt.james.morgan97@gmail.com>
2023-11-30 03:02:47 +0000

    moves all helper functions from index to helper.js

--- helper.js
+++ helper.js
@@ -9,4 +9,40 @@ function getUserByEmail (email, users){
   return undefined;
 };
 
-module.exports = { getUserByEmail }
+//generates random hexidecimal code, you have to enter half of the digits you want
+function generateRandomString(num) {
+  return crypto.randomBytes(num).toString('hex');
+}
+
+
+//this functions makes sure that the user hasn't left blank inputs
+function checkIfEmailAndPasswordAreStrings (email, password){
+  if(email === "" || password === ""){
+    return false
+  }
+  return true;
+}
+
+//takes in two passwords and returns true if they match
+function comparePasswords(inputPassword, userPassword){
+  return bcrypt.compareSync(inputPassword, userPassword);
+}
+
+function urlsForUser(ID, urlDatabase){
+  const userUrls = {}
+
+  for(let id in urlDatabase){
+    if(urlDatabase[id].userID === ID){
+      userUrls[id] = urlDatabase[id];
+    }
+  }
+
+  return userUrls;
+}
+
+module.exports = { getUserByEmail,
+  generateRandomString,
+  checkIfEmailAndPasswordAreStrings,
+  comparePasswords,
+  urlsForUser
+ }
